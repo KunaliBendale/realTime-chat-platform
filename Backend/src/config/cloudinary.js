9631c23b568ea3ad31b5aa9dbaca822e7d9ceffa
KunaliBendale <bendalekunali@gmail.com>
2026-05-22 22:22:12 +0530

    setup cloudinary in backend

---
+++ Backend/src/config/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from "cloudinary"
+
+cloudinary.config({
+    cloud_name: 'dqx4tobjq', 
+    api_key: '596892426984795', 
+    api_secret: 'X9Ia0ql2gl_IeD-JW9wHdDJYSuc'
+})
+
+export {cloudinary}
