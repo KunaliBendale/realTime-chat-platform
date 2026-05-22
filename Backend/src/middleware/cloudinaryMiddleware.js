758ab1a64ecca79bea066c5d546f5eaf4b428c6b
KunaliBendale <bendalekunali@gmail.com>
2026-05-22 22:26:03 +0530

    Added multer middleware to handle image(media) uploads on cloudinary

---
+++ Backend/src/middleware/cloudinaryMiddleware.js
@@ -0,0 +1,13 @@
+import { cloudinary } from "../config/cloudinary.js";
+const uploadOnCloudinary = async(path)=>{
+    const cloudinaryRes = await cloudinary.uploader.upload(path)
+    console.log(cloudinaryRes);
+    return cloudinaryRes;
+}
+
+const deletefromCloudinary=async(public_id)=>{
+    const cloudinaryResponse=await cloudinary.uploader.destroy(public_id)
+    return cloudinaryResponse;
+}
+
+export {uploadOnCloudinary,deletefromCloudinary}
