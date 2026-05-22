5282fcc50999b30ed389a4dce47c7a48dfb67b76
KunaliBendale <bendalekunali@gmail.com>
2026-05-22 22:34:25 +0530

    fix: cloudinary secret leak

--- Backend/src/config/cloudinary.js
+++ Backend/src/config/cloudinary.js
@@ -1,9 +1,9 @@
 import {v2 as cloudinary} from "cloudinary"
 
 cloudinary.config({
-    cloud_name: 'dqx4tobjq', 
-    api_key: '596892426984795', 
-    api_secret: 'X9Ia0ql2gl_IeD-JW9wHdDJYSuc'
+    cloud_name: process.env.CLOUD_NAME , 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET
 })
 
 export {cloudinary}
